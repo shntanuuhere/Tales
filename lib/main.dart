9dbb80adffa32bdc840bfd29aef6dc7738b1823d
shntanuuhere <taleshereco@gmail.com>
2025-04-18 21:03:20 +0530

    New Files

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,28 @@ import 'screens/auth/login_screen.dart';
 import 'screens/auth/register_screen.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/user_details_screen.dart';
-import 'providers/notes_provider.dart';
+import 'providers/notes_provider.dart';import 'package:sentry_flutter/sentry_flutter.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isLoggedIn = FirebaseAuth.instance.currentUser != null;
-  runApp(MyApp(isLoggedIn: isLoggedIn));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://82055cd60d6a1c7363b24c89af1fd90e@o4509174553444352.ingest.us.sentry.io/4509174554558464';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(child: MyApp(isLoggedIn: isLoggedIn))),
+  );
+  // TODO: Remove this line after sending the first sample event to sentry.
+  await Sentry.captureException(Exception('This is a sample exception.'));
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,9 @@ import firebase_core
 import flutter_secure_storage_macos
 import google_sign_in_ios
 import local_auth_darwin
+import package_info_plus
 import path_provider_foundation
+import sentry_flutter
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -22,7 +24,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
